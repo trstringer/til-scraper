df4c7720353d8b6bf8ccbab627b3f9b9e6720796
Thomas Stringer <tr.stringer@outlook.com>
2016-06-20 20:46:28 -0400

    Debug: still trying to fix socket issue

--- public/javascripts/bundle.js
+++ public/javascripts/bundle.js
@@ -47,7 +47,7 @@
 	const React = __webpack_require__(1);
 	const ReactDOM = __webpack_require__(38);
 	const io = __webpack_require__(168);
-	var socket = io(('http://tilscraper.azurewebsites.net:80'));
+	var socket = io(('http://tilscraper.azurewebsites.net'));
 
 	var Tweet = React.createClass({
 	  displayName: 'Tweet',
